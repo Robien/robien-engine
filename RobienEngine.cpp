8bc210820405e64809047d6399c6e0fe48f55113
r.guyard@gmail.com <r.guyard@gmail.com@c76327c8-8b33-3713-03d1-d724dc7a20a2>
2011-05-11 09:09:21 +0000

    ajout d'un hello world + CMakeLists qui permet de compiler sous windows !

---
+++ RobienEngine.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : RobienEngine.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a;
+	puts("Hello World!!!");
+	scanf("%d", &a);
+	return EXIT_SUCCESS;
+}
