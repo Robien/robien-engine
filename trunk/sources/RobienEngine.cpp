0521c5cf10d29dd81f9cbe1a8af9667d83121f41
r.guyard@gmail.com <r.guyard@gmail.com@c76327c8-8b33-3713-03d1-d724dc7a20a2>
2011-05-16 17:44:55 +0000

    TW : la même, mais avec des adds en plus :-O

---
+++ trunk/sources/RobienEngine.cpp
@@ -0,0 +1,147 @@
+/*
+ Copyright (C) 2011 Robien-Engine team
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+ You can contact Robien-Engine team by electronic mail: robien.engine@gmail.com
+ */
+/****************************************
+ *	Project		: robien-engine
+ * 	Name File	: RobienEngine.cpp
+ *
+ *  Created on	: 11 mai 2011
+ *      Author	: Romain
+ ***************************************/
+
+#include <SDL/SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <boost/numeric/conversion/cast.hpp> 
+#include <iostream>
+#include "video/include/WindowManager.h"
+#include "video/include/EventManager.h"
+
+int etape = 0;
+//SDL_Event event;
+void draw_screen(EventManager* E)
+{
+	while (1)
+	{
+
+		etape = (etape + 1) % 360;
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		glTranslatef(0.0f, 0.0f, -5.0f);
+		glRotated(etape, 1, 1, 1);
+		glRotated(etape, 0, 0, 1);
+		glRotated(etape, 1, 0, 0);
+		glRotated(etape, 1, 1, 0);
+		glRotated(etape, 1, 0, 1);
+		glRotated(etape, 1, 0, 3);
+		glBegin(GL_QUADS);
+		glColor3d(1, 0, 0);
+		glVertex3i(1, 1, 1);
+		glColor3d(-1, -9, 0);
+		glColor3d(1, -3, 5);
+		glVertex3i(1, -1, 1);
+		glColor3d(3, 1, -1);
+		glVertex3i(-1, -1, 1);
+		glColor3d(-1, -1, 0);
+		glVertex3i(-1, 1, 1);
+		glColor3d(1, 1, 0);
+		glVertex3i(1, 1, -1);
+		glColor3d(-1, -1, -1);
+		glVertex3i(1, -1, -1);
+		glColor3d(-1, -1, 0);
+		glVertex3i(-1, -1, -1);
+		glColor3d(-1, -1, 0);
+		glVertex3i(-1, 1, -1);
+		glColor3d(1, 0, -1);
+		glVertex3i(1, 1, 1);
+		glColor3d(2, -1, 0);
+		glVertex3i(1, -1, 1);
+		glVertex3i(1, -1, -1);
+		glColor3d(-1, -1, 0);
+		glVertex3i(1, 1, -1);
+		glColor3d(1, 3, 0);
+		glVertex3i(-1, 1, 1);
+		glColor3d(-1, 4, -1);
+		glVertex3i(-1, -1, 1);
+		glColor3d(3, -1, 0);
+		glVertex3i(-1, -1, -1);
+		glColor3d(1, 0, 0);
+		glVertex3i(-1, 1, -1);
+		glColor3d(-1, 0, 6);
+		glVertex3i(-1, 1, -1);
+		glColor3d(-1, 2, -2);
+		glVertex3i(-1, 1, 1);
+		glColor3d(1, -5, 0);
+		glVertex3i(1, 1, 1);
+		glColor3d(-1, -1, 0);
+		glVertex3i(1, 1, -1);
+		glColor3d(-1, -1, 0);
+		glVertex3i(-1, -1, -1);
+		glVertex3i(-1, -1, 1);
+		glColor3d(-3, 2, 0);
+		glVertex3i(1, -1, 1);
+		glColor3d(-1, -2, 3);
+		glVertex3i(1, -1, -1);
+		glEnd();
+		SDL_GL_SwapBuffers();
+		//SDL_PollEvent(&event);
+		if (E->EM_Quit() || E->EM_KeyDown(SDLK_ESCAPE))
+		{
+			return;
+		}
+		/*if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)
+		{
+			//SDL_Quit();
+			return;
+		}*/
+	}
+}
+
+void setup_opengl(int hauteur, int largeur)
+{
+	float ratio = (float) hauteur / (float) largeur;
+	glClearColor(0, 0, 0, 0);
+	glViewport(0, 0, hauteur, largeur);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45, ratio, 0.5, 50);
+	glMatrixMode(GL_MODELVIEW);
+	glEnable(GL_DEPTH_TEST);
+}
+
+int main(int argc, char **argv)
+{
+ using boost::numeric_cast;
+	int hauteur = 800;
+	int largeur = 600;
+	int bits = 32;
+	int flags = 0;
+      int i=42;
+      short s=numeric_cast<short>(i);
+	/*SDL_Init(SDL_INIT_VIDEO);
+	flags = SDL_OPENGL;
+	SDL_SetVideoMode(hauteur, largeur, bits, flags);*/
+	WindowManager fen(hauteur, largeur);
+	EventManager* E = new EventManager();
+	setup_opengl(hauteur, largeur);
+	draw_screen(E);
+	return 0;
+}
+
