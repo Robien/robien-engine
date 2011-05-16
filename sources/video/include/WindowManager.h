aa4c490a161f8f6f9ec947f1730e8e845cb4bc74
r.guyard@gmail.com <r.guyard@gmail.com@c76327c8-8b33-3713-03d1-d724dc7a20a2>
2011-05-16 17:44:55 +0000

    TW : la même, mais avec des adds en plus :-O

---
+++ sources/video/include/WindowManager.h
@@ -0,0 +1,43 @@
+/*
+ Copyright (C) 2011 Robien-Engine team
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+ You can contact Robien-Engine team by electronic mail: robien.engine@gmail.com
+ */
+/****************************************
+ *	Project		: robien-engine
+ * 	Name File	: WindowManager.h
+ *
+ *  Created on	: 13 mai 2011
+ *      Author	: cephise
+ ***************************************/
+
+#ifndef WINDOWMANAGER_H_
+#define WINDOWMANAGER_H_
+
+#include <SDL/SDL.h>
+#include <iostream>
+#include <string>
+
+
+class WindowManager
+{
+public:
+	WindowManager(int height=640 ,int width=480, int bits=32,int flags=SDL_SWSURFACE, std::string name="Robien_Engin Window");
+	virtual ~WindowManager();
+};
+
+#endif /* WINDOWMANAGER_H_ */
