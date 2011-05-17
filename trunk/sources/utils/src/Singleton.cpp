e593578b3b7fc0cebd559ccb1c9be8fdc94125f6
r.guyard@gmail.com <r.guyard@gmail.com@c76327c8-8b33-3713-03d1-d724dc7a20a2>
2011-05-17 21:34:59 +0000

    TW : ajout des Singletons

--- trunk/sources/utils/src/Singleton.cpp
+++ trunk/sources/utils/src/Singleton.cpp
@@ -19,54 +19,12 @@
  */
 /****************************************
  *	Project		: robien-engine
- * 	Name File	: Referenced.h
+ * 	Name File	: Singleton.cpp
  *
  *  Created on	: 17 mai 2011
  *      Author	: Romain
  ***************************************/
 
-#ifndef REFERENCED_H_
-#define REFERENCED_H_
+#include "../include/Singleton.h"
 
-#include <boost/intrusive_ptr.hpp>
 
-class Referenced
-{
-public:
-    Referenced();
-    virtual ~Referenced();
-    friend void intrusive_ptr_add_ref(Referenced* p)
-    {
-        ++p->references;
-    }
-    friend void intrusive_ptr_release(Referenced* p)
-    {
-        if (--p->references == 0)
-            delete p;
-    }
-    inline unsigned int getNombreReference()
-    {
-        return references;
-    }
-    inline bool exist()
-    {
-        return references > 0;
-    }
-    inline bool isUndefined()
-    {
-        return references == 0;
-    }
-
-protected:
-    Referenced& operator=(const Referenced&)
-    {
-        return *this;
-    }
-private:
-    Referenced(const Referenced& other);
-
-private:
-    unsigned int references;
-};
-
-#endif /* REFERENCED_H_ */
